620ff30096d76973195e6db412c53c65ed885551
Jelle Luijkx <j.d.luijkx@tudelft.nl>
2022-03-24 12:10:21 +0100

    ci: reduce test time

--- tests/test_eagerx_examples.py
+++ tests/test_eagerx_examples.py
@@ -3,13 +3,13 @@ from eagerx_examples.example_full_state import example_full_state
 from eagerx_examples.example_pid_only import example_pid_only
 
 
-def test_example_classifier(name="test_classifier", eps=5, eval_eps=2):
+def test_example_classifier(name="test_classifier", eps=1, eval_eps=1):
     example_classifier(name=name, eps=eps, eval_eps=eval_eps)
 
 
-def test_example_full_state(name="test_full_state", eps=5, eval_eps=2):
+def test_example_full_state(name="test_full_state", eps=1, eval_eps=1):
     example_full_state(name=name, eps=eps, eval_eps=eval_eps)
 
 
-def test_example_pid(name="test_pid_only", eps=5, eval_eps=2):
+def test_example_pid(name="test_pid_only", eps=1, eval_eps=1):
     example_pid_only(name=name, eps=eps, eval_eps=eval_eps)
