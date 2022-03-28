85edc8ea3c39a370ac1e7654c5cf651f7fcd9520
Jelle Luijkx <j.d.luijkx@tudelft.nl>
2022-03-28 18:24:47 +0200

    fix: eagerx api changes

--- tests/test_eagerx_examples.py
+++ tests/test_eagerx_examples.py
@@ -1,8 +1,3 @@
-# def test_example_classifier(name="test_classifier", eps=1, eval_eps=1):
-#     from eagerx_examples.example_classifier import example_classifier
-#     example_classifier(name=name, eps=eps, eval_eps=eval_eps)
-
-
 def test_example_full_state(name="test_full_state", eps=1, eval_eps=1):
     from eagerx_examples.example_full_state import example_full_state
     example_full_state(name=name, eps=eps, eval_eps=eval_eps)
