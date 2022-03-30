b09a60e03c0ff09cfcf8b3d9fa6c79aecfc9cb21
Jelle Luijkx <j.d.luijkx@tudelft.nl>
2022-03-30 11:28:25 +0200

    ci: use pyvirtualdisplay to prevent opening render screen

--- tests/test_eagerx_examples.py
+++ tests/test_eagerx_examples.py
@@ -1,6 +1,10 @@
 def test_example_classifier(name="test_classifier", eps=1, eval_eps=1, eps_length=1, classifier_epochs=1):
+    from pyvirtualdisplay import Display
+    display = Display(visible=False, size=(1400, 900))
+    display.start()
     from eagerx_examples.example_classifier import example_classifier
     example_classifier(name=name, eps=eps, eval_eps=eval_eps, eps_length=eps_length, classifier_epochs=classifier_epochs)
+    display.stop()
 
 def test_example_full_state(name="test_full_state", eps=1, eval_eps=1, eps_length=1):
     from eagerx_examples.example_full_state import example_full_state
